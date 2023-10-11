2cd5b1aa9d0d738c54bfae65a5527787182056b1
Adheeee <147314638+Adheeee@users.noreply.github.com>
2023-10-11 15:34:59 +0800

    Add files via upload

---
+++ day3ade.java
@@ -0,0 +1,11 @@
+
+public class day3ade {
+
+    public static void main(String[] args) {
+        int angka1 = 20;
+        int angka2 = 10;
+        int hasil = angka1 + angka2;
+        System.out.println("hasil dari penjumlahan " + hasil);
+    }
+
+}
